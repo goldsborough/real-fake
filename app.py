52d762cd7c936744945ef63fd7881995ced59aee
goldsborough <peter@goldsborough.me>
2017-11-18 01:28:47 -0800

    Added local image data

--- app.py
+++ app.py
@@ -2,19 +2,32 @@ import datetime
 import flask
 import functools
 import json
-import yaml
-import requests
+import random
+
+random.seed(42)
 
 app = flask.Flask('real/fake')
 app.secret_key = '9)P39f.a2C99d9+wH662[=*@'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 60
 
-config = yaml.load(open('config.yaml'))
-print(config)
-LABELS_MAP = requests.get(config['labels_url']).json()
-print(LABELS_MAP)
-KEYS, LABELS = zip(*LABELS_MAP.items())
-print(KEYS, LABELS)
+
+def load_data(labels_file, sample_size=50):
+    labels_map = json.load(open('labels.json'))
+    items = list(labels_map.items())
+    real_items = [item for item in items if item[1]]
+    fake_items = [item for item in items if not item[1]]
+    random.shuffle(real_items)
+    random.shuffle(fake_items)
+    real_items = real_items[:sample_size]
+    fake_items = fake_items[:sample_size]
+    assert len(real_items) == len(fake_items), (len(real_items),
+                                                len(fake_items))
+
+    return zip(*real_items, *fake_items)
+
+
+IMAGES, LABELS = load_data('labels.json')
+
 
 def nocache(view):
     @functools.wraps(view)
@@ -38,7 +51,7 @@ def reset():
 
 
 def get_image_url(image_index):
-    return flask.url_for('static', filename='1.png')
+    return flask.url_for('static', filename=IMAGES[image_index])
 
 
 @app.route('/')
@@ -48,7 +61,6 @@ def index(image_index=0):
     if image_index == 0:
         reset()
     flask.session['image_index'] = image_index
-    print('Rendering template for {}/{}'.format(image_index, len(LABELS)))
     return flask.render_template(
         'index.html',
         image_url=get_image_url(image_index),
@@ -60,8 +72,6 @@ def index(image_index=0):
 def predict():
     prediction = flask.request.json['prediction']
     image_index = flask.session['image_index']
-    print('Received prediction {} for image {}'.format(prediction,
-                                                       image_index))
     if prediction is not None and image_index < len(LABELS):
         if LABELS[image_index]:
             flask.session['real_predictions'] += 1
@@ -89,7 +99,6 @@ def predict():
 
 @app.route('/done')
 def done():
-    print(flask.session)
     return flask.render_template(
         'done.html',
         real_correct=flask.session['real_correct'],
